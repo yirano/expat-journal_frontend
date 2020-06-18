b2d3ea974709bc5686f7a52e16e1a3d7f313235e
Tiffany Arellano <missusfantastic@Missus-Fantastic.local>
2020-06-18 02:14:12 -0400

    Add more to reducer for better state management - separate LOAD_POSTS_PRIVATE/PUBLIC for a more dynamic control

--- src/Action/action.js
+++ src/Action/action.js
@@ -1,7 +1,8 @@
 export const EDIT_POST = 'EDIT_POST'
 export const ADD_POST = 'ADD_POST'
 export const DELETE_POST = 'DELETE_POST'
-export const LOAD_POSTS = 'LOAD_POSTS'
+export const LOAD_POSTS_PUBLIC = 'LOAD_POSTS_PUBLIC'
+export const LOAD_POSTS_PRIVATE = 'LOAD_POSTS_PRIVATE'
 
 export function loadPosts(item) {
 
--- src/Reducer/reducer.js
+++ src/Reducer/reducer.js
@@ -1,22 +1,30 @@
-import { LOAD_POSTS, ADD_POST, DELETE_POST, EDIT_POST } from '../Action/action'
+import {
+  LOAD_POSTS_PRIVATE,
+  LOAD_POSTS_PUBLIC,
+  ADD_POST,
+  DELETE_POST,
+  EDIT_POST,
+} from '../Action/action'
 
 const initialState = {
   data: [],
   isLoading: false,
-  error: ''
+  isLoggedIn: false,
+  error: '',
 }
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-
-    case LOAD_POSTS:
-      return { ...state, ...payload }
+    case LOAD_POSTS_PRIVATE:
+      return { ...state, isLoggedIn: true }
     case ADD_POST:
-      return { ...state, ...payload }
+      return { ...state }
     case DELETE_POST:
-      return { ...state, ...payload }
+      return { ...state }
     case EDIT_POST:
-      return { ...state, ...payload }
+      return { ...state }
+    case LOAD_POSTS_PUBLIC:
+      return { ...state, IsLoggedIn: false }
     default:
       return state
   }
