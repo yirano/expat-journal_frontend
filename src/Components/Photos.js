8ebae173d504572c21ba8898149d3101c1f1d395
yirano <yirano@protonmail.ch>
2020-06-18 13:54:22 -0400

    Remove unused library imports from components

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react"
-import { Route, Link } from 'react-router-dom'
+import React, { useEffect } from "react"
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { loadPosts } from '../Action/action'
