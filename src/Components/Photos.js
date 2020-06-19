c8fd87b65c9335768646f6afc86f58bbbb8e088b
yirano <yirano@protonmail.ch>
2020-06-18 21:00:11 -0400

    Remove unused libraries from components

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react"
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-// import styled from 'styled-components'
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle
-} from 'reactstrap'
 import { loadPosts } from '../Action/action'
 import PhotoCard from './PhotoCard'
-// import Photo from './Photo'
 
 const Photos = ({ images, loadPosts }) => {
 
