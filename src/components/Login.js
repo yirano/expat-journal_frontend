14cf1dffe2152f3e13bdc1e03d3e52a44631dccb
priyanka10it <priyanka10.it@gmail.com>
2020-06-20 16:27:40 -0700

    Fixed image heights in album, photos

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,12 +39,9 @@ export default function Login(props) {
   }
 
   const handleChange = e => {
-<<<<<<< Updated upstream
     formSchema.validate(credentials)
     // setCredentials({ ...credentials, [e.target.name]: e.target.value })
-=======
-    schema.validate(credentials)
->>>>>>> Stashed changes
+    //formSchema.validate(credentials)
     setCredentials({ ...credentials, [e.target.name]: e.target.value })
   }
 
