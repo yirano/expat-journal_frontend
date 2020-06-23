78ee22055a9145c741d4a44584e79f2f0787d387
yirano <yirano@protonmail.ch>
2020-06-22 23:24:13 -0400

    Set up Authorization

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,13 +6,15 @@ import { Link } from 'react-router-dom'
 
 
 export default function Login(props) {
+
+
   const [credentials, setCredentials] = useState({
-    userid: "",
+    username: "",
     password: ""
   })
 
   const [formState, setFormState] = useState({
-    userid: "",
+    username: "",
     password: ""
   })
 
@@ -21,35 +23,24 @@ export default function Login(props) {
   const [buttonDisabled, setButtonDisabled] = useState(true)
 
   const [errors, setErrors] = useState({
-    userid: "",
+    username: "",
     password: ""
   })
+
   const handleLogin = e => {
-    e.preventDefault()
+    axiosWithAuth().post('https://expat-journal2.herokuapp.com/api/auth/login', credentials)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.token)
+        props.history.push('/photos')
+      })
     props.history.push('/photos')
-
-    // axiosWithAuth().post('http://localhost:5000/api/login', credentials)
-    //   .then(res => {
-    //     console.log(res)
-    //     localStorage.setItem('token', res.data.payload)
-    //     props.history.push('/photos')
-    //   })
-    // props.setLoggedState(true)
-    // props.history.push('/photos')
-    // localStorage.setItem('loggedState', true)
-  }
-
-  const handleChange = e => {
-    formSchema.validate(credentials)
-    // setCredentials({ ...credentials, [e.target.name]: e.target.value })
-    //formSchema.validate(credentials)
-    setCredentials({ ...credentials, [e.target.name]: e.target.value })
   }
 
   //* Login Page Validation-REACT I
 
   const formSchema = yup.object().shape({
-    userid: yup.string().required("UserId is a required field"),
+    username: yup.string().required("UserId is a required field"),
     password: yup
       .string()
       .required("Please enter your password")
@@ -65,16 +56,6 @@ export default function Login(props) {
       setButtonDisabled(!isFormValid)
     })
   }, [formState])
-
-
-  const formSubmit = e => {
-    e.preventDefault()
-
-    setFormState({
-      userid: "",
-      password: "",
-    })
-  }
 
   const validateChange = e => {
     yup
@@ -97,37 +78,36 @@ export default function Login(props) {
 
   const inputChange = e => {
     e.persist()
-
-
     const newFormData = {
       ...formState,
       [e.target.name]:
-        e.target.value 
+        e.target.value
     }
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
 
-    validateChange(e) 
-    setFormState(newFormData) 
+    validateChange(e)
+    setFormState(newFormData)
   }
 
   return (
-    
-    <Form onSubmit={formSubmit} >
-      <h1>Welcome to Expat Journal!!</h1><br/>
+
+    <Form onSubmit={handleLogin} >
+      <h1>Welcome to Expat Journal!!</h1><br />
       <h5>If you're a new user, please register.</h5><br />
-<h5>If you've already registered, please login to view posts.</h5>
+      <h5>If you've already registered, please login to view posts.</h5>
       {serverError ? <p className="error">{serverError}</p> : null}
       <Label for="userid">
         <legend>UserId</legend>
         <Input
           id="userid"
           type="text"
-          name="userid"
+          name="username"
           placeholder="Please enter userid here"
           onChange={inputChange}
-          value={formState.userid}
-          
+          value={formState.username}
+
         />
-        {errors.userid.length > 0 ? <p className="error">{errors.userid}</p> : null}
+        {errors.username.length > 0 ? <p className="error">{errors.username}</p> : null}
       </Label><br />
       <Label htmlFor="password">
         <legend>Password</legend>
@@ -151,7 +131,7 @@ export default function Login(props) {
         <Button type="submit">Register</Button>
       </Link>
     </Form>
-    
+
 
   )
 }
