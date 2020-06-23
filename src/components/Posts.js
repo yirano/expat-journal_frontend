c0e245b21ee27214c82bc1e4ead8575667e98069
yirano <yirano@protonmail.ch>
2020-06-23 14:02:53 -0400

    Allow user to post images

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react'
-// import { Link } from 'react-router-dom'
-import { Form, Label, Input, FormText, Button } from 'reactstrap'
+import { Form, Label, Input, Button } from 'reactstrap'
 import * as yup from 'yup'
+import axiosWithAuth from '../axiosWithAuth/axiosWithAuth'
 
 const Posts = () => {
-  // const [post, setPost] = useState([])
 
   const [serverError, setServerError] = useState("")
 
   const [formState, setFormState] = useState({
     title: "",
     description: "",
-    date: "",
-    url: ""
+    photo_url: ""
   })
 
   const [buttonDisabled, setButtonDisabled] = useState(true)
@@ -20,21 +18,17 @@ const Posts = () => {
   const [errors, setErrors] = useState({
     title: "",
     description: "",
-    date: "",
-    url: ""
+    photo_url: ""
   })
 
   const formSchema = yup.object().shape({
-    title: yup.string().required("Title is a required field"), // must include name or else error
+    title: yup.string().required("Title is a required field"),
     description: yup.string().required("Description is a required fiels"),
-    date: yup.date().required("Please enter date"),
-    url: yup.string().required("Please enter a valid URL").matches(/[https://]/)
+    photo_url: yup.string().required("Please enter a valid URL").matches(/[https://]/)
   })
 
   useEffect(() => {
-    console.log(
-      "checking to see if all values in form state follows the rules set in formSchema"
-    )
+
     formSchema.isValid(formState).then(isFormValid => {
       console.log("is form valid?", isFormValid)
       setButtonDisabled(!isFormValid) // disabled= false if form is valid
@@ -43,30 +37,28 @@ const Posts = () => {
 
 
   const formSubmit = e => {
-    e.preventDefault() // <form> onSubmit has default behavior from HTML!
-
+    e.preventDefault()
+    axiosWithAuth().post('/posts/user/1', formState)
+      .then(res => console.log(res))
+      .catch(err => console.log(err.response))
     setFormState({
       title: "",
       description: "",
-      date: "",
-      url: ""
+      photo_url: ""
     })
   }
 
   const validateChange = e => {
-    // get the value out of schema at key "e.target.name" --> "name="
     yup
       .reach(formSchema, e.target.name)
       .validate(e.target.value) // value in input
       .then(inputIsValid => {
-        // if inputIsValid is true, then erase any errors in error state at that key/value in errors
         setErrors({
           ...errors,
           [e.target.name]: ""
         })
       })
       .catch(err => {
-        // if failing validation, set error in state
         setErrors({
           ...errors,
           [e.target.name]: err.errors[0]
@@ -74,21 +66,17 @@ const Posts = () => {
       })
   }
 
-  // onChange function
   const inputChange = e => {
-    // use persist with async code
-    e.persist() // necessary because we're passing the event asyncronously and we need it to exist even after this function completes (which will complete before validateChange finishes)
-    // console.log("input changed!", e.target.value)
-    // console.log("name of input that fired event", e.target.name) // [e.target.name]: e.target.value --> computed props
+    e.persist()
 
     const newFormData = {
       ...formState,
       [e.target.name]:
-        e.target.value // // remember value of the checkbox is in "checked" and all else is "value"
+        e.target.value
     }
 
-    validateChange(e) // for each change in input, do inline validation
-    setFormState(newFormData) // update state with new data
+    validateChange(e)
+    setFormState(newFormData)
   }
 
   return (
@@ -121,34 +109,20 @@ const Posts = () => {
           <p className="error">{errors.description}</p>
         ) : null}
       </Label>
-      <br />
-      <Label htmlFor="date">
-        <legend>Date</legend>
-        <Input
-          type="date"
-          name="date"
-          id="date"
-          placeholder="Please select date here"
-          value={formState.date}
-          onChange={inputChange}
-        />
-        {errors.date.length > 0 ? (
-          <p className="error">{errors.date}</p>
-        ) : null}
-      </Label>
+
       <br />
       <Label htmlFor="url">
         <legend>Image URL</legend>
         <Input
           type="url"
-          name="url"
-          id="url"
+          name="photo_url"
+          id="photo_url"
           placeholder="Please enter image URL here"
-          value={formState.url}
+          value={formState.photo_url}
           onChange={inputChange}
         />
-        {errors.url.length > 0 ? (
-          <p className="error">{errors.url}</p>
+        {errors.photo_url.length > 0 ? (
+          <p className="error">{errors.photo_url}</p>
         ) : null}
       </Label>
       <br />
